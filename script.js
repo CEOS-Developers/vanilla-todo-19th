493c67035de5e8bc2667b7671c81ffc86f1a6e55
Programming-Seungwan <swsj4480@gmail.com>
2024-03-13 02:59:09 +0900

    Feat : 사용자가 input 박스에 내용을 입력하고 엔터 키를 누르면 todo list에 들어가는 함수 로직 작성
    1. 아직 localStorage와의 동기화는 진행되지 않았음
    2. dom 생성이 되기 이전에 dom 관련 api 함수를 사용해서 실제 동작 시에는 에러가 발생함

--- script.js
+++ script.js
@@ -8,25 +8,55 @@ let doneList =
     ? []
     : localStorage.getItem('doneList');
 
-// dom 엘리먼트들을 픽해서 배열에 있던 것들을 추가해줌
 const todoListAreaDiv = document
   .querySelector('.todo-area')
   .querySelector('.div');
 
-todoList.forEach((todo) => {
-  const showingTodoDiv = document.createElement('div');
-  showingTodoDiv.classList.add('showingTodo');
-  showingTodoDiv.innerText = todo;
-  todoListAreaDiv.appendChild(showingTodoDiv);
-});
-
 const doneListAreaDiv = document
   .querySelector('.done-area')
   .querySelector('.div');
+
+const inputBox = document.querySelector('.todo-input');
+
+const todayDateInput = document.querySelector('.date-input');
+
+// 새로운 요소를 할일 UI 쪽에 추가하는 함수. 새로운 dom 요소를 생성하여 UI에 반영함
+function addNewTodoListToShowingUI(newTodo) {
+  const showingTodoDiv = document.createElement('div');
+  showingTodoDiv.classList.add('showingTodo');
+  showingTodoDiv.innerText = newTodo;
+  todoListAreaDiv.appendChild(showingTodoDiv);
+}
+
+todoList.forEach((todo) => {
+  addNewTodoListToShowingUI(todo);
+});
 
 doneList.forEach((done) => {
   const showingDoneDiv = document.createElement('div');
   showingDoneDiv.classList.add('showingDone');
   showingDoneDiv.innerText = done;
   todoListAreaDiv.appendChild(showingDoneDiv);
+});
+
+// 사용자가 input box에 내용을 입력하고 엔터 키를 눌러 제출할 때 트리거 되는 함수. 내부에서 유효성 검사를 수행한다.
+function handleSubmitInputBoxByEnterKey() {
+  const inputBoxValue = inputBox.value.trim();
+  if (inputBoxValue === '') {
+    alert('내용을 입력한 뒤에 todo 리스트에 등록하세요');
+    return;
+  }
+
+  todoList.push(inputBoxValue);
+  addNewTodoListToShowingUI(inputBoxValue); // UI에 반영해 주었음
+}
+
+//사용자가 input box에 내용을 입력하고 submit 버튼을 눌러 제출할 때 트리거 되는 함수. 역시 내부에서 유효성 검사를 수행.
+function handleSubmitInputBoxBySubmitButton() {}
+
+// input box에 포커싱이 되어 있고 엔터키가 눌리면 UI 추가가 되어야 함
+inputBox.addEventListener('keydown', (event) => {
+  if (event.keyCode === 13) {
+    handleSubmitInputBoxByEnterKey();
+  }
 });
