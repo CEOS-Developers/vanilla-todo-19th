7180dd33970f4602e9885f8cfe2c9632e2f5de68
Programming-Seungwan <swsj4480@gmail.com>
2024-03-13 14:53:12 +0900

    Docs : localStorage 처음 DOM 마운트 시, 자료구조 문제 해결중

--- app.js
+++ app.js
@@ -1,13 +1,3 @@
-// 웹 페이지가 처음 렌더링 되는 시점에 로컬 스토리지로 부터 불러옴. 아직 존재하지 않는다면 빈 배열을 할당(삼항 연산자 이용)
-let todoList =
-  localStorage.getItem('todoList') === null
-    ? []
-    : localStorage.getItem('todoList');
-let doneList =
-  localStorage.getItem('doneList') === null
-    ? []
-    : localStorage.getItem('doneList');
-
 const todoListAreaDiv = document
   .querySelector('.todo-area')
   .querySelector('.showing-list-area');
@@ -19,6 +9,26 @@ const doneListAreaDiv = document
 const inputBox = document.querySelector('.todo-input');
 
 const todayDateInput = document.querySelector('.date-input');
+const currentDate = new Date();
+const formattedDate = currentDate.toISOString().slice(0, 10);
+todayDateInput.value = formattedDate;
+
+// 현재 선택된 날짜의 YYYY-MM-DD 문자열을 반환하는 함수
+function getNowDateFormatString() {
+  return todayDateInput.value;
+}
+
+// 웹 페이지가 처음 렌더링 되는 시점에 로컬 스토리지로 부터 불러옴. 아직 존재하지 않는다면 빈 배열을 할당(삼항 연산자 이용)
+const firstDomMountedDateFormatString = getNowDateFormatString();
+
+let todoList =
+  localStorage.getItem('todoList')[firstDomMountedDateFormatString] === null
+    ? []
+    : localStorage.getItem('todoList')[firstDomMountedDateFormatString];
+let doneList =
+  localStorage.getItem('doneList')[firstDomMountedDateFormatString] === null
+    ? []
+    : localStorage.getItem('doneList')[firstDomMountedDateFormatString];
 
 // 새로운 요소를 할일 UI 쪽에 추가하는 함수. 새로운 dom 요소를 생성하여 UI에 반영함
 function addNewTodoListToShowingUI(newTodo) {
@@ -40,7 +50,7 @@ doneList.forEach((done) => {
 });
 
 // 사용자가 input box에 내용을 입력하고 엔터 키를 눌러 제출할 때 트리거 되는 함수. 내부에서 유효성 검사를 수행한다.
-function handleSubmitInputBoxByEnterKey() {
+function handleSubmitInputBoxByEnterKeyOrSubmitButton() {
   const inputBoxValue = inputBox.value.trim();
   if (inputBoxValue === '') {
     alert('내용을 입력한 뒤에 todo 리스트에 등록하세요');
@@ -52,12 +62,19 @@ function handleSubmitInputBoxByEnterKey() {
   inputBox.value = '';
 }
 
-//사용자가 input box에 내용을 입력하고 submit 버튼을 눌러 제출할 때 트리거 되는 함수. 역시 내부에서 유효성 검사를 수행.
-function handleSubmitInputBoxBySubmitButton() {}
-
 // input box에 포커싱이 되어 있고 엔터키가 눌리면 UI 추가가 되어야 함
 inputBox.addEventListener('keydown', (event) => {
   if (event.keyCode === 13) {
-    handleSubmitInputBoxByEnterKey();
+    addTodoItemToLocalStrage(inputBox.value.trim());
+    handleSubmitInputBoxByEnterKeyOrSubmitButton();
   }
 });
+
+function addTodoItemToLocalStrage(todoItem) {
+  const nowDateFormatString = getNowDateFormatString();
+  if (localStorage.getItem('todoList') === null) {
+    const newTodoListObject = {};
+    newTodoListObject[nowDateFormatString] = [todoItem];
+    localStorage.setItem('todoList', JSON.stringify(newTodoListObject));
+  }
+}
