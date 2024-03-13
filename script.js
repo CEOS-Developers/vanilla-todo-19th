93de70d8b9496a14b83dd316db4467c6da153cc9
Rose <120544840+Rose-my@users.noreply.github.com>
2024-03-13 17:59:31 +0900

    [etc] 주석추가

--- script.js
+++ script.js
@@ -1,69 +1,8 @@
 document.addEventListener('DOMContentLoaded', function () {
-  const saveBtn = document.querySelector('.saveBtn');
-  const closeBtn = document.querySelector('.closeBtn');
-  const editModalBackground = document.querySelector('.editModalBackground');
-
   let countSchedule = 0;
   let countDone = 0;
 
-  saveBtn.addEventListener('click', function () {
-    const inputDescription = document.getElementById('addDescription').value;
-    if (inputDescription.trim() !== '') {
-      addTodoItem('todaySchedule', inputDescription);
-      closeEditSchedule();
-    } else {
-      alert('Please enter a description.');
-    }
-  });
-
-  closeBtn.addEventListener('click', function () {
-    closeEditSchedule();
-  });
-
-  function addTodoItem(id, description) {
-    const doneIcon = document.createElement('i');
-    doneIcon.textContent = 'SCHEDULE';
-
-    doneIcon.addEventListener('click', function () {
-      if (doneIcon.textContent === 'SCHEDULE') {
-        doneIcon.textContent = 'DONE';
-        makeHistoryElement('done');
-        li.classList.add('done');
-      } else {
-        doneIcon.textContent = 'SCHEDULE';
-        makeHistoryElement('schedule');
-        li.classList.remove('done');
-        li.classList.add('schedule');
-      }
-    });
-
-    const ul = document.querySelector('.list ul');
-    const li = document.createElement('li');
-    li.append(description, doneIcon);
-
-    ul.appendChild(li);
-    makeHistoryElement(id);
-  }
-
-  function closeEditSchedule() {
-    document.getElementById('addDescription').value = '';
-    editModalBackground.style.display = 'none';
-  }
-
-  const editBtn = document.querySelector('footer button');
-  editBtn.addEventListener('click', function () {
-    editModalBackground.style.display = 'flex';
-  });
-
-  function getTodayDate() {
-    const today = new Date();
-    const year = today.getFullYear();
-    const month = String(today.getMonth() + 1).padStart(2, '0');
-    const day = String(today.getDate()).padStart(2, '0');
-    const dateElement = document.querySelector('.date');
-    dateElement.textContent = `${year}.${month}.${day}`;
-  }
-
+  // current schedule&done
   function makeHistoryElement(id) {
     if (id == 'schedule') {
       countSchedule += 1;
@@ -103,6 +42,74 @@ document.addEventListener('DOMContentLoaded', function () {
     historyElement.append(historyH2, historyDiv);
   }
 
+  // 오늘 날짜
+  function getTodayDate() {
+    const today = new Date();
+    const year = today.getFullYear();
+    const month = String(today.getMonth() + 1).padStart(2, '0');
+    const day = String(today.getDate()).padStart(2, '0');
+    const dateElement = document.querySelector('.date');
+    dateElement.textContent = `${year}.${month}.${day}`;
+  }
+
+  // 모달 띄우기, 닫기
+  const editModalBackground = document.querySelector('.editModalBackground');
+  const editBtn = document.querySelector('footer button');
+
+  editBtn.addEventListener('click', function () {
+    editModalBackground.style.display = 'flex';
+  });
+
+  function closeEditSchedule() {
+    document.getElementById('addDescription').value = '';
+    editModalBackground.style.display = 'none';
+  }
+
+  // 모달 저장, 닫기 버튼 이벤트함수
+  const saveBtn = document.querySelector('.saveBtn');
+  const closeBtn = document.querySelector('.closeBtn');
+
+  saveBtn.addEventListener('click', function () {
+    const inputDescription = document.getElementById('addDescription').value;
+    if (inputDescription.trim() !== '') {
+      addTodoItem('todaySchedule', inputDescription);
+      closeEditSchedule();
+    } else {
+      alert('Please enter a description.');
+    }
+  });
+
+  closeBtn.addEventListener('click', function () {
+    closeEditSchedule();
+  });
+
+  // 스케줄 리스트 추가하기
+  function addTodoItem(id, description) {
+    const currentText = document.createElement('i');
+    currentText.textContent = 'SCHEDULE';
+
+    currentText.addEventListener('click', function () {
+      if (currentText.textContent === 'SCHEDULE') {
+        currentText.textContent = 'DONE';
+        makeHistoryElement('done');
+        li.classList.add('done');
+      } else {
+        currentText.textContent = 'SCHEDULE';
+        makeHistoryElement('schedule');
+        li.classList.remove('done');
+        li.classList.add('schedule');
+      }
+    });
+
+    const ul = document.querySelector('.list ul');
+    const li = document.createElement('li');
+    li.append(description, currentText);
+
+    ul.appendChild(li);
+    makeHistoryElement(id);
+  }
+
+  // schedule, done 필터링 함수
   let scheduleChecked = true;
   let doneChecked = true;
 
@@ -124,13 +131,14 @@ document.addEventListener('DOMContentLoaded', function () {
     scheduleChecked = !scheduleChecked;
     handleFilter();
   });
-  
+
   const doneFilter = document.querySelector('label[for="done"]');
   doneFilter.addEventListener('click', function () {
     doneChecked = !doneChecked;
     handleFilter();
   });
 
+  // 렌더링
   makeHistoryElement();
   getTodayDate();
 });
