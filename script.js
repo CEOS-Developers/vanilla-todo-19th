c4f5866c88351d89c9c2877506a9134760d944f4
CSE-pebble <CSE.pebble@gmail.com>
2024-03-14 18:42:55 +0900

    Style: Add Progress Bar HTML&CSS

--- script.js
+++ script.js
@@ -14,24 +14,42 @@ const moveItem = (todoListNode) => {
   const isDone = todoListNode.classList.contains("done");
   const todoUl = document.querySelector(".todo-box__todo ul");
   const doneUl = document.querySelector(".todo-box__done ul");
+
+  // ❗ ????아니 걍 appendChild하면 복사될줄 알았는데 자동으로 삭제도 되네???이게 뭔일임 질문하자
   if (isDone) {
     doneUl.appendChild(todoListNode);
-    // ❗ ????아니 걍 appendChild하면 복사될줄 알았는데 자동으로 삭제도 되네???이게 뭔일임 질문하자
   } else {
     todoUl.appendChild(todoListNode);
   }
+};
+
+// Todo Progress Bar 업데이트
+const updateItemCount = () => {
+  console.log("update");
+  const todoUl = document.querySelector(".todo-box__todo ul");
+  const doneUl = document.querySelector(".todo-box__done ul");
+
+  const todoCount = todoUl.childElementCount;
+  const doneCount = doneUl.childElementCount;
+  console.log(`${doneCount} / ${doneCount + todoCount}`);
 };
 
 // Todo 또는 Done class 토글 함수
 const toggleTodo = (e) => {
   const todoListNode = e.target.parentElement;
   todoListNode.classList.toggle("done");
+  // 이동
   moveItem(todoListNode);
+  // Progess bar 업데이트
+  updateItemCount();
 };
 
 const deleteItem = (e) => {
   const todoListNode = e.target.parentElement.parentElement;
+  // 삭제
   todoListNode.remove();
+  // Progess bar 업데이트
+  updateItemCount();
 };
 
 // 할일 추가 함수 ❗내부 분리할 필요 있을까?
@@ -57,6 +75,9 @@ const addTodo = () => {
   // 추가
   const todoUl = document.querySelector(".todo-box__todo ul");
   todoUl.appendChild(todoListNode);
+
+  // Progess bar 업데이트
+  updateItemCount();
 
   // input value 초기화
   todoInput.value = "";
