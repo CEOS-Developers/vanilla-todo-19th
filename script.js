e3d6225037aa11a3b9baa39446a760b73bcbaeb9
CSE-pebble <CSE.pebble@gmail.com>
2024-03-14 15:53:04 +0900

    Feat: Add Adding Todo Function - 할일 추가 기능 구현

--- script.js
+++ script.js
@@ -9,11 +9,37 @@ const getTodayDate = () => {
   return `${year}.${month}.${day}`;
 };
 
+// 할일 추가 함수
+const addTodo = () => {
+  // input에 입력한 값 가져오기
+  const todoInput = document.querySelector(".input-box input");
+  const todoInputText = todoInput.value;
+
+  // li element 생성 후 input 값 세팅
+  const todoListNode = document.createElement("li");
+  todoListNode.textContent = todoInputText;
+
+  // 추가
+  const todoUl = document.querySelector(".todo-box__todo ul");
+  todoUl.appendChild(todoListNode);
+
+  // input value 초기화
+  todoInput.value = "";
+};
+
 // 첫 화면 렌더링용 함수
 const init = () => {
   // 오늘 날짜 세팅
   const date = document.querySelector(".date");
   date.textContent = getTodayDate();
+
+  // 엔터키 or 플러스 버튼을 누르면 할일 추가
+  const todoInput = document.querySelector(".input-box input");
+  const plusBtn = document.querySelector(".input-box button");
+  todoInput.addEventListener("keyup", function handleEnter(e) {
+    if (e.keyCode === 13) addTodo();
+  });
+  plusBtn.addEventListener("click", addTodo);
 };
 
 init();
