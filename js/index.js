14b669af63db5139fcab531c69b36c49f8df2fb6
youdame <112458620+youdame@users.noreply.github.com>
2024-03-15 22:48:44 +0900

    Feat : 개수, 유저 이름 보여주기 기능 구현

--- js/index.js
+++ js/index.js
@@ -3,51 +3,58 @@ import { $ } from './util.js';
 const getTodayDate = () => {
   // 오늘 날짜
   const today = new Date();
-  // 년도
+
+  // 년, 월, 일
   const year = today.getFullYear();
-  // 월
   const month = (today.getMonth() + 1).toString();
-  // 일
   const date = today.getDate().toString();
 
-  const dayToString = ['월', '화', '수', '목', '금', '토', '일'];
+  //요일 배열
+  const dayToString = ['일', '월', '화', '수', '목', '금', '토'];
 
   const day = dayToString[today.getDay()];
   return `${year}년 ${month}월 ${date}일 ${day}요일`;
 };
 
-const addTodoList = (e) => {
-  e.preventDefault();
-
-  const inputValue = $('.input').value.trim();
-  if (!inputValue) return;
-
+const createTodo = (text) => {
   const newTodoItem = document.createElement('li');
 
-  // 동그라미 아이콘
   const circleIcon = document.createElement('i');
   circleIcon.classList.add('fa-regular', 'fa-circle', 'cursor-pointer');
+
   newTodoItem.appendChild(circleIcon);
 
-  // 할 일 텍스트
-  const todoText = document.createTextNode(inputValue);
+  const todoText = document.createTextNode(text);
   newTodoItem.appendChild(todoText);
 
-  // 쓰레기통 아이콘
   const trashIcon = document.createElement('i');
   trashIcon.classList.add('fa-solid', 'fa-trash-can', 'cursor-pointer');
   newTodoItem.appendChild(trashIcon);
 
-  // 투두에 추가
-  const todoList = $('.todo-list');
-  todoList.append(newTodoItem);
-
-  circleIcon.addEventListener('click', moveToDoneList);
-  trashIcon.addEventListener('click', deleteItem);
-  $('.input').value = '';
+  return newTodoItem;
 };
 
-const moveToDoneList = (e) => {
+const addTodo = (e) => {
+  e.preventDefault();
+
+  const inputValue = $('.input').value.trim();
+  if (!inputValue) {
+    $('.error').textContent = '내용을 입력해주세요';
+    return;
+  }
+  $('.error').textContent = '';
+
+  const newTodoItem = createTodo(inputValue);
+  $('.todo-list').append(newTodoItem);
+  $('.input').value = '';
+
+  newTodoItem.querySelector('.fa-circle').addEventListener('click', moveTodo);
+  newTodoItem.querySelector('.fa-trash-can').addEventListener('click', deleteTodo);
+
+  updateListCount();
+};
+
+const moveTodo = (e) => {
   const circleIcon = e.target;
   const clickedTodo = circleIcon.parentNode;
 
@@ -63,12 +70,29 @@ const moveToDoneList = (e) => {
     circleIcon.classList.add('fa-circle');
     $('.todo-list').append(clickedTodo);
   }
+
+  updateListCount();
 };
 
-const deleteItem = (e) => {
+const deleteTodo = (e) => {
   const clickedTodo = e.target.parentNode;
   clickedTodo.remove();
+
+  updateListCount();
 };
 
+const updateListCount = () => {
+  $('.todo-count').textContent = '/ ' + $('.todo-list').childElementCount + '개';
+  $('.done-count').textContent = '/' + $('.done-list').childElementCount + '개';
+};
+
+const showUserName = () => {
+  const userName = prompt('이름을 알려주세요!');
+  if (userName) {
+    $('.userName').textContent = userName + '의 ';
+  }
+};
+
+showUserName();
 $('.date').textContent = getTodayDate();
-$('.input-form').addEventListener('submit', addTodoList);
+$('.input-form').addEventListener('submit', addTodo);
