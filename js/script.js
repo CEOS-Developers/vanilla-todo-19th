9385d1afc365842ad4d4a3262da51c3cd2491430
youdame <112458620+youdame@users.noreply.github.com>
2024-03-15 20:56:37 +0900

    Feat: 할 일 추가, 이동, 삭제 구현

--- js/script.js
+++ js/script.js
@@ -1,6 +1,5 @@
 import { $ } from './util.js';
 
-const dayToString = ['월', '화', '수', '목', '금', '토', '일'];
 const getTodayDate = () => {
   // 오늘 날짜
   const today = new Date();
@@ -11,8 +10,65 @@ const getTodayDate = () => {
   // 일
   const date = today.getDate().toString();
 
+  const dayToString = ['월', '화', '수', '목', '금', '토', '일'];
+
   const day = dayToString[today.getDay()];
   return `${year}년 ${month}월 ${date}일 ${day}요일`;
 };
 
-$('.date').innerText = getTodayDate();
+const addTodoList = (e) => {
+  e.preventDefault();
+
+  const inputValue = $('.input').value.trim();
+  if (!inputValue) return;
+
+  const newTodoItem = document.createElement('li');
+
+  // 동그라미 아이콘
+  const circleIcon = document.createElement('i');
+  circleIcon.classList.add('fa-regular', 'fa-circle', 'cursor-pointer');
+  newTodoItem.appendChild(circleIcon);
+
+  // 할 일 텍스트
+  const todoText = document.createTextNode(inputValue);
+  newTodoItem.appendChild(todoText);
+
+  // 쓰레기통 아이콘
+  const trashIcon = document.createElement('i');
+  trashIcon.classList.add('fa-solid', 'fa-trash-can', 'cursor-pointer');
+  newTodoItem.appendChild(trashIcon);
+
+  // 투두에 추가
+  const todoList = $('.todo-list');
+  todoList.append(newTodoItem);
+
+  circleIcon.addEventListener('click', moveToDoneList);
+  trashIcon.addEventListener('click', deleteItem);
+  $('.input').value = '';
+};
+
+const moveToDoneList = (e) => {
+  const circleIcon = e.target;
+  const clickedTodo = circleIcon.parentNode;
+
+  // 만약 .todo 안에 있는 항목이라면
+  if (clickedTodo.closest('.todo-list')) {
+    circleIcon.classList.remove('fa-circle');
+    circleIcon.classList.add('fa-check-circle');
+    $('.done-list').append(clickedTodo);
+  }
+  // 만약 .done-list 안에 있는 항목이라면
+  else if (clickedTodo.closest('.done-list')) {
+    circleIcon.classList.remove('fa-check-circle');
+    circleIcon.classList.add('fa-circle');
+    $('.todo-list').append(clickedTodo);
+  }
+};
+
+const deleteItem = (e) => {
+  const clickedTodo = e.target.parentNode;
+  clickedTodo.remove();
+};
+
+$('.date').textContent = getTodayDate();
+$('.input-form').addEventListener('submit', addTodoList);
