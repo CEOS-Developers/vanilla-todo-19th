efa97830b605797ad9c52120c3a0e4e5f55bf667
jinnyleeis <101055312+jinnyleeis@users.noreply.github.com>
2024-03-15 23:10:36 +0900

    feat : js에서 조작할 돔 요소 접근
    유일한 요소 -> document.getElementById로 접근

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //CEOS 19기 프론트엔드 파이팅🔥 ദ്ദി˶ˊᵕˋ˵)
+
+document.addEventListener('DOMContentLoaded', function () {
+	/* 조작할 DOM 요소 아이디로 참조 ->
+     달력 / 섹션 추가 버튼 / 섹션명 입력 인풋 필드 / 섹션 컨테이너 */
+	const datePicker = document.getElementById('datePicker');
+	const addSectionBtn = document.getElementById('addSectionBtn');
+	const sectionInput = document.getElementById('sectionInput');
+	const todoSectionsDiv = document.getElementById('todoSections');
+});
